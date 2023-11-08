736c85ed9fff253c68c1256a608624bcb0090551
JANG-JAEWON <jewon119@gmail.com>
2023-11-08 09:01:03 +0900

    fix: CORS parameter 추가

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
