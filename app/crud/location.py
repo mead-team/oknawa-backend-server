9ca661074c7698f96498762529d725e8365507e0
jewon119 <jewon119@gmail.com>
2023-12-03 17:50:00 +0900

    fix: temp_point_location에서 가져오던 데이터 DB에서 가져오도록 api 수정

--- app/crud/location.py
+++ app/crud/location.py
@@ -1,3 +1,15 @@
+from app.models.location import PopularMeetingLocation
+
+
 def create_popular_meeting_location(db, popular_meeting_location_obj):
     db.add_all(popular_meeting_location_obj)
     db.commit()
+
+
+def get_popular_meeting_location(db):
+    place_data = (
+        db.query(PopularMeetingLocation)
+        .filter(PopularMeetingLocation.deleted_at == None)
+        .all()
+    )
+    return place_data
