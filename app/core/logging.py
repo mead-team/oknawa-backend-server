f5e110857690da5f4ffad63851c0d6ed8f81e0cc
deight93 <deight8311@gmail.com>
2023-12-14 17:45:13 +0900

    refact: 로깅 백그라운드 테스크 제거

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,7 +1,7 @@
 import logging
 
-from fastapi import FastAPI, Request, Response
-from starlette.background import BackgroundTask
+from fastapi import Request
+from starlette.concurrency import iterate_in_threadpool
 from starlette.types import Message
 
 # logging
@@ -29,35 +29,19 @@ async def set_body(request: Request, body: bytes):
     request._receive = receive
 
 
-async def logging_temp(request, call_next):
-    req_body = await request.body()
-    await set_body(request, req_body)
+async def get_body(request: Request) -> bytes:
+    body = await request.body()
+    await set_body(request, body)
+    return body
+
+
+async def logging_print(request: Request, call_next):
+    await set_body(request, await request.body())
     response = await call_next(request)
-
-    res_body = b""
-    async for chunk in response.body_iterator:
-        res_body += chunk
-
-    if (
-        response.headers["content-type"]
-        != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    ):
-        res_body = res_body.decode("utf-8")
-        req_body = req_body.decode("utf-8")
-
-    if response.status_code == 200:
-        return Response(
-            content=res_body,
-            status_code=response.status_code,
-            headers=dict(response.headers),
-            media_type=response.media_type,
-        )
-    else:
-        task = BackgroundTask(log_error, req_body, res_body)
-        return Response(
-            content=res_body,
-            status_code=response.status_code,
-            headers=dict(response.headers),
-            media_type=response.media_type,
-            background=task,
-        )
+    response_body = [chunk async for chunk in response.body_iterator]
+    response.body_iterator = iterate_in_threadpool(iter(response_body))
+    if response.status_code != 200:
+        req_body = await get_body(request)
+        res_body = (b"".join(response_body)).decode()
+        log_error(response.status_code, req_body.decode(), res_body)
+    return response
